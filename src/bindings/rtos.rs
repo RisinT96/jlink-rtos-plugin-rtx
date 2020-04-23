2ca6831110d1ac9f0606f3b414797481a313178a
Tal Risin <risint96@gmail.com>
2020-04-23 22:14:26 +0300

    Implemented thread detection logic

--- src/bindings/rtos.rs
+++ src/bindings/rtos.rs
@@ -13,27 +13,35 @@ use crate::host::api;
 /* ------------------------------------- Types ---------------------------------------------------------------------- */
 
 #[derive(Debug)]
-pub struct Thread<'a> {
+pub struct Thread {
     name: String,
-    id: u8,
+    id: u32,
     stack_frame: u8,
     stack_base: u32,
     stack_size: u32,
     stack_pointer: u32,
-    priority: &'a str,
-    state: &'a str,
+    priority: &'static str,
+    state: &'static str,
     pub(in crate::bindings::rtos) thread_next: u32,
     pub(in crate::bindings::rtos) delay_next: u32,
 }
 
+struct ThreadReadyList {
+    next_thread_addr: u32,
+}
+
+struct ThreadDelayList {
+    next_thread_addr: u32,
+}
+
 #[derive(Debug)]
-pub struct RtxInfo<'a> {
-    threads: Vec<Thread<'a>>,
+pub struct RtxInfo {
+    threads: Vec<Thread>,
 }
 
 /* ------------------------------------- Thread Implementations ----------------------------------------------------- */
-impl Thread<'_> {
-    pub fn new(address: u32) -> Result<Thread<'static>, i32> {
+impl Thread {
+    pub fn new(address: u32) -> Result<Thread, i32> {
         trace!("Loading Thread Info from {:#X}", address);
 
         let thread_info: osRtxThread_t = api::read_mem(address)?;
@@ -48,7 +56,7 @@ impl Thread<'_> {
         Ok(Thread {
             name: name,
 
-            id: thread_info.id,
+            id: address,
             stack_frame: thread_info.stack_frame,
             stack_base: api::convert_u32(thread_info.stack_mem as u32)?,
             stack_size: api::convert_u32(thread_info.stack_size)?,
@@ -124,12 +132,12 @@ impl Thread<'_> {
     }
 }
 
-impl std::fmt::Display for Thread<'_> {
+impl std::fmt::Display for Thread {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
         if (self.name.len() != 0) {
             write!(
                 f,
-                "{name} <{id:0<3}> [{priority}] ({state})",
+                "{name} <{id:0<8X}> [{priority}] ({state})",
                 id = self.id,
                 name = self.name,
                 priority = self.priority,
@@ -138,11 +146,123 @@ impl std::fmt::Display for Thread<'_> {
         } else {
             write!(
                 f,
-                "<{id:0<3}> [{priority}] ({state})",
+                "<{id:0<8X}> [{priority}] ({state})",
                 id = self.id,
                 priority = self.priority,
                 state = self.state
             )
         }
+    }
+}
+
+impl ThreadReadyList {
+    pub fn new(address: u32) -> ThreadReadyList {
+        ThreadReadyList {
+            next_thread_addr: address,
+        }
+    }
+}
+
+impl ThreadDelayList {
+    pub fn new(address: u32) -> ThreadDelayList {
+        ThreadDelayList {
+            next_thread_addr: address,
+        }
+    }
+}
+
+impl Iterator for ThreadReadyList {
+    type Item = Thread;
+
+    fn next(&mut self) -> Option<Thread> {
+        if (self.next_thread_addr as *const u32 == std::ptr::null()) {
+            return None;
+        }
+
+        let thread = Thread::new(self.next_thread_addr).unwrap();
+
+        self.next_thread_addr = api::convert_u32(thread.thread_next).unwrap();
+
+        return Some(thread);
+    }
+}
+
+impl Iterator for ThreadDelayList {
+    type Item = Thread;
+
+    fn next(&mut self) -> Option<Thread> {
+        if (self.next_thread_addr as *const u32 == std::ptr::null()) {
+            return None;
+        }
+
+        let thread = Thread::new(self.next_thread_addr).unwrap();
+
+        self.next_thread_addr = api::convert_u32(thread.delay_next).unwrap();
+
+        return Some(thread);
+    }
+}
+
+/* ------------------------------------- RtxInfo Implementations ---------------------------------------------------- */
+impl RtxInfo {
+    pub fn new(address: u32) -> Result<RtxInfo, i32> {
+        trace!("Loading RTOS Info from {:#X}", address);
+        let rtx_info: osRtxInfo_t = api::read_mem(address)?;
+
+        let version = api::convert_u32(rtx_info.version)?;
+        let os_id = api::convert_u32(rtx_info.os_id as u32)?;
+
+        let mut name = String::new();
+
+        // Load RTX Name, if possible
+        if (os_id as *const i8 != std::ptr::null()) {
+            name = api::read_string(os_id, 256)?;
+            debug!("Found RTX '{}', Version {}", name, version);
+        } else {
+            debug!("Found RTX Version {}", version);
+        }
+
+        let mut threads: Vec<Thread> = Vec::new();
+
+        // If we ran before the kernel was even initialized, exit, there are zero threads.
+        match rtx_info.kernel.state as i32 {
+            osKernelState_t_osKernelInactive | osKernelState_t_osKernelError => {
+                debug!("RTX Kernel not initialized yet!");
+                return Ok(RtxInfo { threads });
+            }
+            _ => (),
+        };
+
+        debug!("Loading currently running thread");
+        threads.push(Thread::new(api::convert_u32(
+            rtx_info.thread.run.curr as u32,
+        )?)?);
+        debug!("Currently running thread: {}", threads[0]);
+        
+        debug!("Loading ready list threads");
+        for thread in
+        ThreadReadyList::new(api::convert_u32(rtx_info.thread.ready.thread_list as u32)?)
+        {
+            debug!("Found thread: {}", thread);
+            threads.push(thread);
+        }
+        
+        debug!("Loading delay list threads");
+        for thread in ThreadDelayList::new(api::convert_u32(rtx_info.thread.delay_list as u32)?) {
+            debug!("Found thread: {}", thread);
+            threads.push(thread);
+        }
+        
+        debug!("Loading wait list threads");
+        for thread in ThreadDelayList::new(api::convert_u32(rtx_info.thread.wait_list as u32)?) {
+            debug!("Found thread: {}", thread);
+            threads.push(thread);
+        }
+
+        Ok(RtxInfo { threads })
+    }
+
+    pub fn threads_num(&self) -> usize {
+        self.threads.len()
     }
 }
