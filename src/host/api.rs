ad482c82f28f65fd2c73b34487e395b4b24c95db
Tal Risin <risint96@gmail.com>
2020-04-25 23:20:16 +0300

    Reworked GDB API

--- src/host/api.rs
+++ src/host/api.rs
@@ -20,34 +20,17 @@ macro_rules! ensure {
     };
 }
 
-/// Only data that is C FFI safe should have this trait.
-pub unsafe trait ReprC {}
-
 /// Pointer to struct holding the API provided by the GDB Server, initialized in `RTOS_Init`.jlink
 /// Must not be changed after that.
-static mut GDB_API: GdbApi = GdbApi {
-    pfFree: None,
-    pfAlloc: None,
-    pfRealloc: None,
-    pfLogOutf: None,
-    pfDebugOutf: None,
-    pfWarnOutf: None,
-    pfErrorOutf: None,
-    pfReadMem: None,
-    pfReadU8: None,
-    pfReadU16: None,
-    pfReadU32: None,
-    pfWriteMem: None,
-    pfWriteU8: None,
-    pfWriteU16: None,
-    pfWriteU32: None,
-    pfLoad16TE: None,
-    pfLoad24TE: None,
-    pfLoad32TE: None,
-    pfReadReg: None,
-    pfWriteReg: None,
-    Dummy: null_mut(),
-};
+static mut GDB_API: Option<GdbApi> = None;
+
+fn gdb_api_get() -> Option<&'static GdbApi> {
+    unsafe { GDB_API.as_ref() }
+}
+
+unsafe fn gdb_api_set(gdb_api: GdbApi) {
+    GDB_API = Some(gdb_api);
+}
 
 pub fn init(p_api: *const GdbApi) -> Result<(), ()> {
     if p_api.is_null() {
@@ -55,7 +38,7 @@ pub fn init(p_api: *const GdbApi) -> Result<(), ()> {
     }
 
     unsafe {
-        GDB_API = *p_api;
+        gdb_api_set(*p_api);
     }
 
     Ok(())
@@ -64,78 +47,105 @@ pub fn init(p_api: *const GdbApi) -> Result<(), ()> {
 /* ------------------------------------- GDB Server API Wrappers ---------------------------------------------------- */
 
 pub unsafe fn free(ptr: *mut u8) {
-    match GDB_API.pfFree {
-        Some(f) => f(ptr as *mut c_void),
-        None => (),
+    if let Some(gdb_api) = gdb_api_get() {
+        if let Some(f) = gdb_api.pfFree {
+            f(ptr as *mut c_void);
+        }
     }
 }
 
 pub unsafe fn alloc(size: usize) -> *mut u8 {
-    match GDB_API.pfAlloc {
-        Some(f) => f(size as c_uint) as *mut u8,
-        None => null_mut(),
+    if let Some(gdb_api) = gdb_api_get() {
+        if let Some(f) = gdb_api.pfAlloc {
+            return f(size as c_uint) as *mut u8;
+        }
     }
+
+    null_mut()
 }
 
 pub unsafe fn realloc(ptr: *mut u8, size: usize) -> *mut u8 {
-    match GDB_API.pfRealloc {
-        Some(f) => f(ptr as *mut c_void, size as c_uint) as *mut u8,
-        None => null_mut(),
+    if let Some(gdb_api) = gdb_api_get() {
+        if let Some(f) = gdb_api.pfRealloc {
+            return f(ptr as *mut c_void, size as c_uint) as *mut u8;
+        }
     }
+
+    null_mut()
 }
 
 pub fn print(s: &str) {
-    let c_to_print = CString::new(s).unwrap();
-
-    unsafe {
-        match GDB_API.pfLogOutf {
-            Some(f) => f(c_to_print.as_ptr()),
-            None => (),
+    if let Some(gdb_api) = gdb_api_get() {
+        if let Some(f) = gdb_api.pfLogOutf {
+            if let Ok(c_string) = CString::new(s) {
+                unsafe {
+                    f(c_string.as_ptr());
+                }
+            }
         }
     }
 }
 
 pub fn print_debug(s: &str) {
-    let c_to_print = CString::new(s).unwrap();
-
-    unsafe {
-        match GDB_API.pfDebugOutf {
-            Some(f) => f(c_to_print.as_ptr()),
-            None => (),
+    if let Some(gdb_api) = gdb_api_get() {
+        if let Some(f) = gdb_api.pfDebugOutf {
+            if let Ok(c_string) = CString::new(s) {
+                unsafe {
+                    f(c_string.as_ptr());
+                }
+            }
         }
     }
 }
 
 pub fn print_warning(s: &str) {
-    let c_to_print = CString::new(s).unwrap();
-
-    unsafe {
-        match GDB_API.pfWarnOutf {
-            Some(f) => f(c_to_print.as_ptr()),
-            None => (),
+    if let Some(gdb_api) = gdb_api_get() {
+        if let Some(f) = gdb_api.pfWarnOutf {
+            if let Ok(c_string) = CString::new(s) {
+                unsafe {
+                    f(c_string.as_ptr());
+                }
+            }
         }
     }
 }
 
 pub fn print_error(s: &str) {
-    let c_to_print = CString::new(s).unwrap();
-
-    unsafe {
-        match GDB_API.pfErrorOutf {
-            Some(f) => f(c_to_print.as_ptr()),
-            None => (),
+    if let Some(gdb_api) = gdb_api_get() {
+        if let Some(f) = gdb_api.pfErrorOutf {
+            if let Ok(c_string) = CString::new(s) {
+                unsafe {
+                    f(c_string.as_ptr());
+                }
+            }
         }
     }
 }
 
 pub fn read_mem<T>(addr: u32) -> Result<T, i32> {
-    let mut value = std::mem::MaybeUninit::uninit();
+    if let Some(gdb_api) = gdb_api_get() {
+        if let Some(f) = gdb_api.pfReadMem {
+            let mut value = std::mem::MaybeUninit::uninit();
+            let size = std::mem::size_of::<T>();
 
-    let size = std::mem::size_of::<T>();
-    unsafe {
-        if let Some(f) = GDB_API.pfReadMem {
-            if f(addr, value.as_mut_ptr() as *mut c_char, size as c_uint) as usize == size {
-                return Ok(value.assume_init());
+            unsafe {
+                if f(addr, value.as_mut_ptr() as *mut c_char, size as c_uint) as usize == size {
+                    return Ok(value.assume_init());
+                }
+            }
+        }
+    }
+
+    Err(GDB_ERR)
+}
+
+pub fn read_mem_by_len(addr: u32, len: usize) -> Result<Vec<u8>, i32> {
+    if let Some(gdb_api) = gdb_api_get() {
+        if let Some(f) = gdb_api.pfReadMem {
+            let mut buff: Vec<u8> = vec![0u8; len];
+
+            if unsafe { f(addr, buff.as_mut_ptr() as *mut c_char, len as c_uint) } as usize == len {
+                return Ok(buff);
             }
         }
     }
@@ -144,122 +154,132 @@ pub fn read_mem<T>(addr: u32) -> Result<T, i32> {
 }
 
 pub fn read_u8(addr: u32) -> Result<u8, i32> {
-    let mut buff: u8 = 0;
+    if let Some(gdb_api) = gdb_api_get() {
+        if let Some(f) = gdb_api.pfReadU8 {
+            let mut buff: u8 = 0;
 
-    unsafe {
-        match GDB_API.pfReadU8 {
-            Some(f) => match f(addr, &mut buff) as i32 {
-                GDB_OK => Ok(buff),
-                e => Err(e),
-            },
-            None => Err(GDB_ERR),
+            if unsafe { f(addr, &mut buff) } as i32 == GDB_OK {
+                return Ok(buff);
+            }
         }
     }
+
+    Err(GDB_ERR)
 }
 
 pub fn read_u16(addr: u32) -> Result<u16, i32> {
-    let mut buff: u16 = 0;
+    if let Some(gdb_api) = gdb_api_get() {
+        if let Some(f) = gdb_api.pfReadU16 {
+            let mut buff: u16 = 0;
 
-    unsafe {
-        match GDB_API.pfReadU16 {
-            Some(f) => match f(addr, &mut buff) as i32 {
-                GDB_OK => Ok(buff),
-                e => Err(e),
-            },
-            None => Err(GDB_ERR),
+            if unsafe { f(addr, &mut buff) } as i32 == GDB_OK {
+                return Ok(buff);
+            }
         }
     }
+
+    Err(GDB_ERR)
 }
 
 pub fn read_u32(addr: u32) -> Result<u32, i32> {
-    let mut buff: u32 = 0;
+    if let Some(gdb_api) = gdb_api_get() {
+        if let Some(f) = gdb_api.pfReadU32 {
+            let mut buff: u32 = 0;
 
-    unsafe {
-        match GDB_API.pfReadU32 {
-            Some(f) => match f(addr, &mut buff) as i32 {
-                GDB_OK => Ok(buff),
-                e => Err(e),
-            },
-            None => Err(GDB_ERR),
+            if unsafe { f(addr, &mut buff) } as i32 == GDB_OK {
+                return Ok(buff);
+            }
         }
     }
+
+    Err(GDB_ERR)
 }
 
 pub fn write_mem<T>(addr: u32, data: &T) -> Result<(), i32> {
-    unsafe {
-        match GDB_API.pfWriteMem {
-            None => Err(GDB_ERR),
-            Some(f) => match f(
-                addr,
-                data as *const T as *const c_char,
-                std::mem::size_of::<T>() as c_uint,
-            ) {
-                GDB_OK => Ok(()),
-                e => Err(e),
-            },
+    if let Some(gdb_api) = gdb_api_get() {
+        if let Some(f) = gdb_api.pfWriteMem {
+            if unsafe {
+                f(
+                    addr,
+                    data as *const T as *const c_char,
+                    std::mem::size_of::<T>() as c_uint,
+                )
+            } == GDB_OK
+            {
+                return Ok(());
+            }
         }
     }
+
+    Err(GDB_ERR)
 }
 
 pub fn write_u8(addr: u32, data: u8) {
-    unsafe {
-        match GDB_API.pfWriteU8 {
-            Some(f) => f(addr, data),
-            None => (),
+    if let Some(gdb_api) = gdb_api_get() {
+        if let Some(f) = gdb_api.pfWriteU8 {
+            unsafe {
+                f(addr, data);
+            }
         }
     }
 }
 
 pub fn write_u16(addr: u32, data: u16) {
-    unsafe {
-        match GDB_API.pfWriteU16 {
-            Some(f) => f(addr, data),
-            None => (),
+    if let Some(gdb_api) = gdb_api_get() {
+        if let Some(f) = gdb_api.pfWriteU16 {
+            unsafe {
+                f(addr, data);
+            }
         }
     }
 }
 
 pub fn write_u32(addr: u32, data: u32) {
-    unsafe {
-        match GDB_API.pfWriteU32 {
-            Some(f) => f(addr, data),
-            None => (),
+    if let Some(gdb_api) = gdb_api_get() {
+        if let Some(f) = gdb_api.pfWriteU32 {
+            unsafe {
+                f(addr, data);
+            }
         }
     }
 }
 
 pub fn convert_u16(data: u16) -> Result<u16, i32> {
-    unsafe {
-        match GDB_API.pfLoad16TE {
-            Some(f) => Ok(f(&data as *const u16 as *const u8) as u16),
-            None => Err(GDB_ERR),
+    if let Some(gdb_api) = gdb_api_get() {
+        if let Some(f) = gdb_api.pfLoad16TE {
+            return Ok(unsafe { f(&data as *const u16 as *const u8) } as u16);
         }
     }
+
+    Err(GDB_ERR)
 }
 
 pub fn convert_u32(data: u32) -> Result<u32, i32> {
-    unsafe {
-        match GDB_API.pfLoad32TE {
-            Some(f) => Ok(f(&data as *const u32 as *const u8)),
-            None => Err(GDB_ERR),
+    if let Some(gdb_api) = gdb_api_get() {
+        if let Some(f) = gdb_api.pfLoad32TE {
+            return Ok(unsafe { f(&data as *const u32 as *const u8) } as u32);
         }
     }
+
+    Err(GDB_ERR)
 }
 
 pub fn read_reg(reg_index: u32) -> Result<u32, i32> {
-    unsafe {
-        match GDB_API.pfReadReg {
-            Some(f) => Ok(f(reg_index)),
-            None => Err(GDB_ERR),
+    if let Some(gdb_api) = gdb_api_get() {
+        if let Some(f) = gdb_api.pfReadReg {
+            return Ok(unsafe { f(reg_index) });
         }
     }
+
+    Err(GDB_ERR)
 }
 
 pub fn write_reg(reg_index: u32, data: u32) {
-    unsafe {
-        match GDB_API.pfWriteReg {
-            Some(f) => f(reg_index, data),
-            None => (),
+    if let Some(gdb_api) = gdb_api_get() {
+        if let Some(f) = gdb_api.pfWriteReg {
+            unsafe {
+                f(reg_index, data);
+            }
         }
     }
 }
@@ -267,26 +287,12 @@ pub fn write_reg(reg_index: u32, data: u32) {
 /* ------------------------------------- GDB Server API Extension --------------------------------------------------- */
 
 pub fn read_string(addr: u32, max_len: usize) -> Result<String, i32> {
-    let mut temp_buff: Vec<u8> = vec![0u8; max_len];
-
-    unsafe {
-        if let Some(f) = GDB_API.pfReadMem {
-            if f(
-                addr,
-                temp_buff.as_mut_ptr() as *mut c_char,
-                max_len as c_uint,
-            ) as usize
-                != max_len
-            {
-                return Err(GDB_ERR);
+    if let Ok(buff) = read_mem_by_len(addr, max_len) {
+        // Find null terminator - '\0'
+        if let Some(pos) = memchr::memchr(0, &buff) {
+            if let Ok(string) = std::str::from_utf8(&buff[..pos]) {
+                return Ok(string.to_owned());
             }
-        }
-    }
-
-    // Find null terminator - '\0'
-    if let Some(pos) = memchr::memchr(0, &temp_buff) {
-        if let Ok(string) = std::str::from_utf8(&temp_buff[..pos]) {
-            return Ok(string.to_owned());
         }
     }
 
