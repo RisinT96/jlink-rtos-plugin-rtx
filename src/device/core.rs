49f9aeda569360948858c8d221346df46cd00d8d
Tal Risin <risint96@gmail.com>
2020-05-01 14:17:25 +0300

    Added definiton of All Cortex-M MCU registers

--- src/device/core.rs
+++ src/device/core.rs
@@ -3,8 +3,82 @@
 use crate::api;
 use crate::bindings::jlink;
 
+/* ------------------------------------- Types ---------------------------------------------------------------------- */
+
 struct Core {
     has_fpu: bool,
+}
+
+type Reg = u32;
+
+type FloatReg = f32;
+
+#[repr(C)]
+#[derive(Copy, Clone)]
+/// General registers of Cortex-M0 to M3 MCUs
+pub struct GeneralRegs {
+    r0: Reg,
+    r1: Reg,
+    r2: Reg,
+    r3: Reg,
+    r4: Reg,
+    r5: Reg,
+    r6: Reg,
+    r7: Reg,
+    r8: Reg,
+    r9: Reg,
+    r10: Reg,
+    r11: Reg,
+    r12: Reg,
+    sp: Reg,
+    lr: Reg,
+    pc: Reg,
+    xpsr: Reg,
+    msp: Reg,
+    psp: Reg,
+    primask: Reg,
+    basepri: Reg,
+    faultmask: Reg,
+    control: Reg,
+}
+
+#[repr(C)]
+#[derive(Copy, Clone)]
+/// General registers of Cortex-M4 MCUs
+pub struct GeneralRegsFpu {
+    general: GeneralRegs,
+    s0: FloatReg,
+    s1: FloatReg,
+    s2: FloatReg,
+    s3: FloatReg,
+    s4: FloatReg,
+    s5: FloatReg,
+    s6: FloatReg,
+    s7: FloatReg,
+    s8: FloatReg,
+    s9: FloatReg,
+    s10: FloatReg,
+    s11: FloatReg,
+    s12: FloatReg,
+    s13: FloatReg,
+    s14: FloatReg,
+    s15: FloatReg,
+    s16: FloatReg,
+    s17: FloatReg,
+    s18: FloatReg,
+    s19: FloatReg,
+    s20: FloatReg,
+    s21: FloatReg,
+    s22: FloatReg,
+    s23: FloatReg,
+    s24: FloatReg,
+    s25: FloatReg,
+    s26: FloatReg,
+    s27: FloatReg,
+    s28: FloatReg,
+    s29: FloatReg,
+    s30: FloatReg,
+    s31: FloatReg,
 }
 
 /* ------------------------------------- State variable and getter/setter ------------------------------------------- */
